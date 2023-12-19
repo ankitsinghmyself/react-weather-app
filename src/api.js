ca71733bd1406d54d7fab053ffd31026ee7c9cee
Ankit singh <ankitsinghmyself@gmail.com>
2023-12-19 23:21:57 +0530

    d

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const GEO_API_URL = "http://api.openweathermap.org/geo/1.0";
+export const GEO_API_URL = "https://api.openweathermap.org/geo/1.0";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 export const WEATHER_API_KEY = "ca68e880c4481b78662ba837496f209f"; 
